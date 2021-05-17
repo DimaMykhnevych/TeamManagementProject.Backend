a7d7d2e668a09aa8130459e1479ca8bebf4e8407
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-05-17 10:34:48 +0300

    Remove SecretKey In Transaction Entity

--- TeamManagement/TeamManagement.DataLayer/Domain/Models/Transaction.cs
+++ TeamManagement/TeamManagement.DataLayer/Domain/Models/Transaction.cs
@@ -5,7 +5,6 @@ namespace TeamManagement.DataLayer.Domain.Models
     public class Transaction
     {
         public Guid Id { get; set; }
-        public string SecretKey { get; set; }
         public string PublicKey { get; set; }
         public DateTime TransactionDate { get; set; }
 
